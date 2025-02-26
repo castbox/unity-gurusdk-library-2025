51e70e42fd6a97adddfd21f7c2a961119384a4dc
Jenkins <castbox@castbox-mac-studio-bj.local>
2025-02-26 21:56:49 +0800

    Make version 1.5.0 on  2025/02/26 21:56:48  by push

--- 1.5.0/com.guru.unity.buildtool/Editor/BuildTool/AppBuilder.cs
+++ 1.5.0/com.guru.unity.buildtool/Editor/BuildTool/AppBuilder.cs
@@ -355,7 +355,7 @@ namespace Guru.Editor
 	
 	        if (!string.IsNullOrEmpty(teamId))
 	        {
-		        PlayerSettings.iOS.appleEnableAutomaticSigning = false;
+		        PlayerSettings.iOS.appleEnableAutomaticSigning = true;
 		        PlayerSettings.iOS.appleDeveloperTeamID = teamId;
 	        }
 	        
